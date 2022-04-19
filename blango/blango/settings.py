6614148fe99e4c49eea4f67907cfc1b2d80282e1
Valiantsina Valiantsina <valechka_iv@mail.ru>
2022-04-19 12:58:13 +0000

    Finish security ad passwords

--- blango/blango/settings.py
+++ blango/blango/settings.py
@@ -123,6 +123,14 @@ class Dev(Configuration):
         },
     ]
 
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
+
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -188,4 +196,4 @@ class Dev(Configuration):
         },
     }
 
-
+     
